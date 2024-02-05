66894c6487abeebc1bc3ed90908ae58e311c199e
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2024-02-05 21:40:28 +0900

    [Bronze III] Title: 개표, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int N;
+string arr;
+int a, b;
+
+int main(){
+    cin.tie(0);
+    cout.tie(0);
+    cin >> N;
+    cin >> arr;
+    for(int i = 0; i < arr.size(); i++){
+        if(arr[i] == 'A') a++;
+        else b++;
+    }
+    if(a > b) cout << "A";
+    else if(b > a) cout << "B";
+    else cout << "Tie";
+    return 0;
+}
