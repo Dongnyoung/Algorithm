79beb997020834d0d3bdba272122d2c5ebf8f9e0
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2024-02-12 12:26:34 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+    fastio;
+    queue<int> Q;
+    int n;
+    cin >> n;
+    for(int i =1; i<=n ; i++){
+        Q.push(i);
+    }
+    while (Q.size()>1){
+        Q.pop();
+        Q.push(Q.front());
+        Q.pop();
+    }
+    cout << Q.front() << '\n' ;
+}
+
