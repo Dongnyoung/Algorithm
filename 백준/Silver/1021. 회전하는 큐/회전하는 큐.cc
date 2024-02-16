783681dc98f064168c7bf76687b7004666f3ec73
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2024-02-16 17:05:39 +0900

    [Silver III] Title: 회전하는 큐, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() {
+    fastio;
+    deque<int> dq;
+    int n, m;
+    cin >> n >> m;
+
+    int cnt = 0;
+
+    // 1~n까지 수를 넣어준다
+    for (int i = 1; i <= n; i++)
+        dq.push_back(i);
+
+    while (m--) {
+        // 위치 값 받기
+        int t;
+        cin >> t;
+
+        int idx = 0;
+
+        // 위치 찾기
+        for (int i = 0; i < dq.size(); i++) {
+            if (dq.front() == t) {
+                idx = i;
+                break;
+            }
+            dq.push_back(dq.front());
+            dq.pop_front();
+        }
+
+        // 회전 횟수 계산
+        cnt += min<int>(idx, dq.size() - idx);
+
+        // 현재 위치의 숫자 제거
+        dq.pop_front();
+    }
+
+    cout << cnt << '\n';
+
+    return 0;
+}
