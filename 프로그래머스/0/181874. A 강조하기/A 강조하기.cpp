eebe07a84f71ba943679498f0ab36619a67f2637
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2024-07-26 12:20:18 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    
+    
+    for(int i =0;i<myString.size();i++){
+        
+        if(myString[i] =='a')
+            myString[i] = toupper(myString[i]);
+        else if(myString[i]!='A')
+            myString[i] = tolower(myString[i]);
+    }
+    answer = myString;
+    return answer;
+}
+    
