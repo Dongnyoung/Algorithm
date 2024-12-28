8e93cb7c41d8627332910df7078bf7dba8af3a15
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2024-12-28 18:57:35 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.58 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,14 +1,12 @@
-//영어문자면 숫자로 바꾸고 그냥 숫자면 그대로
-//문자열 배열을 생성하자
+
 
 class Solution {
     public int solution(String s) {
-        int answer=0;
-        String [] eng = {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
-        for(int i =0;i<eng.length;i++)
-            if(s.contains(eng[i]))
-                s = s.replace(eng[i],Integer.toString(i));
-        answer = Integer.parseInt(s);
-        return answer;
+      //숫자 문자열
+        String [] number = {"zero","one","two","three","four","five","six","seven","eight","nine","ten"};
+        for(int i=0;i<number.length;i++){
+            s = s.replaceAll(number[i],Integer.toString(i));
+        }
+        return Integer.parseInt(s);
     }
 }
