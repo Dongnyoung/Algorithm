3765187842d5aab4595242f146e1e0e65aa7feb6
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2025-03-12 13:57:23 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 34.91 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,23 +1,19 @@
 class Solution {
-    private int zeroCount;
-
-    // 이진 변환 함수
-    private String translate(String s) {
-        zeroCount += s.length() - s.replace("0", "").length(); // 0의 개수 세기
-        s = s.replace("0", ""); // 0 제거
-        return Integer.toBinaryString(s.length()); // 변환된 이진 문자열 반환
+    private static int zeroCount;
+    //이진변환 함수
+    private static String translate(String s){
+        zeroCount += s.length() - s.replace("0","").length(); //0개수세기
+        s=s.replaceAll("0","");
+        return Integer.toBinaryString(s.length());
+        
     }
-
     public int[] solution(String s) {
-        int transCount = 0;
-        zeroCount = 0;
-
-        // 1이 될 때까지 반복
-        while (!s.equals("1")) {
-            s = translate(s);
-            transCount++;
+        zeroCount=0;
+        int count=0;
+        while(!s.equals("1")){
+            s= translate(s);
+            count++;
         }
-
-        return new int[]{transCount, zeroCount};
+        return new int[]{count,zeroCount};
     }
 }
