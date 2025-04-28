9a69c2daac311c6fcf25001ca8db937419c6a57c
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2025-04-28 18:44:51 +0900

    [Gold IV] Title: LCS 2, Time: 124 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Main {
+    private static int dp[][] = new int[1001][1001];
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String n = scan.nextLine();
+        String m = scan.nextLine();
+        for (int i = 1; i < n.length()+1; i++) {
+            for (int j = 1; j < m.length()+1; j++) {
+                
+                    // 같으면 왼쪽위 +1
+                    if (n.charAt(i-1) == m.charAt(j-1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1;
+                        
+                    }
+                    // 다르면 왼쪽,위쪽중 max
+                    else {
+                        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                    }
+                }
+        }
+        int i=n.length();
+        int j=m.length();
+        StringBuilder sb = new StringBuilder();
+        while(i>0 && j >0){
+            if(n.charAt(i-1) == m.charAt(j-1)){
+                sb.append(n.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if(dp[i][j-1] > dp[i-1][j]){
+                j--;
+            }
+            else {
+                i--;
+            }
+        }
+        
+        System.out.println(dp[n.length()][m.length()]);
+        System.out.println(sb.reverse().toString());
+        scan.close();
+        }
+        
+
+}
