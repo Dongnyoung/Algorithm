a5eb0718bfa44b08e8fa3620c19add71297884ae
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2025-08-04 14:35:08 +0900

    [level 3] Title: 양과 늑대, Time: 4.90 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,57 +1,51 @@
 import java.util.*;
-
 class Solution {
-    int maxSheep = 0;
-
-    static class Node {
-        int animal; // 0: 양, 1: 늑대
+    int answer = 0;
+    static class Node{
+        int animal;
         List<Integer> children = new ArrayList<>();
-
-        public Node(int animal) {
-            this.animal = animal;
+        public Node(int animal){
+            this.animal=animal;
         }
     }
-
     public int solution(int[] info, int[][] edges) {
+        
         Node[] nodes = new Node[info.length];
-
-        // 노드 생성
-        for (int i = 0; i < info.length; i++) {
+        //노드 생성
+        for(int i=0;i<info.length;i++){
             nodes[i] = new Node(info[i]);
         }
-
-        // 부모-자식 연결
-        for (int[] edge : edges) {
-            nodes[edge[0]].children.add(edge[1]);
+        //부모-자식 연결
+        for(int edge[]:edges){
+            int parent = edge[0];
+            int child = edge[1];
+            nodes[parent].children.add(child);
         }
-
+        
+        //자식 그릇
         List<Integer> nextNodes = new ArrayList<>();
         nextNodes.addAll(nodes[0].children);
-
-        dfs(nodes, 0, 1, 0, nextNodes);
-
-        return maxSheep;
+        
+        dfs(nodes,0,1,0,nextNodes);
+        
+        return answer;
     }
-
-    void dfs(Node[] nodes, int current, int sheep, int wolf, List<Integer> nextNodes) {
-        maxSheep = Math.max(maxSheep, sheep);
-
-        for (int i = 0; i < nextNodes.size(); i++) {
+    void dfs(Node[] nodes, int current, int sheepCount,int wolfCount,List<Integer> nextNodes){
+        answer = Math.max(answer,sheepCount);
+        for(int i=0;i<nextNodes.size();i++){
             int next = nextNodes.get(i);
-            int nextSheep = sheep;
-            int nextWolf = wolf;
-
-            if (nodes[next].animal == 0) nextSheep++;
+            int nextSheep = sheepCount;
+            int nextWolf = wolfCount;
+            if(nodes[next].animal==0) nextSheep++;
             else nextWolf++;
-
-            if (nextWolf >= nextSheep) continue;
-
-            // 다음 방문 가능 노드 업데이트
-            List<Integer> newList = new ArrayList<>(nextNodes);
-            newList.remove(i);
-            newList.addAll(nodes[next].children);
-
-            dfs(nodes, next, nextSheep, nextWolf, newList);
+            
+            if(nextSheep<=nextWolf) continue;
+            
+            List<Integer> nextNodeChild = new ArrayList<>(nextNodes);
+            nextNodeChild.remove(i);
+            nextNodeChild.addAll(nodes[next].children);
+            
+            dfs(nodes, next, nextSheep,nextWolf,nextNodeChild);
         }
     }
 }
