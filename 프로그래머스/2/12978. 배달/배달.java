dec592d59fd1c212545be18cc2a9c647b36a9db4
DongNyoung Lee <121621378+Dongnyoung@users.noreply.github.com>
2025-08-24 12:24:09 +0900

    [level 2] Title: 배달, Time: 5.98 ms, Memory: 90.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    private static class Node{
+        int cost;
+        int vertex;
+        public Node(int vertex,int cost){
+            this.vertex=vertex;
+            this.cost=cost;
+        }
+    }
+    private static ArrayList<Node>[] nodeList;
+    private static int dist[];
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+        nodeList= new ArrayList[N+1];
+        for(int i=1;i<nodeList.length;i++){
+            nodeList[i] = new ArrayList<>();
+        }
+        for(int []edge : road){
+            nodeList[edge[0]].add(new Node(edge[1],edge[2]));
+            nodeList[edge[1]].add(new Node(edge[0],edge[2]));
+        }
+        dist = new int[N+1];
+        for(int i=1;i<=N;i++){
+            dist[i]=Integer.MAX_VALUE;
+        }
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)->Integer.compare(o1.cost,o2.cost));
+        pq.add(new Node(1,0));
+        dist[1]=0;
+        while(!pq.isEmpty()){
+            Node now = pq.poll();
+            for(Node next: nodeList[now.vertex]){
+                if(dist[next.vertex]>next.cost+now.cost){
+                    dist[next.vertex]=next.cost+now.cost;
+                    pq.add(new Node(next.vertex,dist[next.vertex]));
+                }
+            }
+        }
+        
+        for(int i=1;i<=N;i++){
+            if(dist[i]<=K){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
