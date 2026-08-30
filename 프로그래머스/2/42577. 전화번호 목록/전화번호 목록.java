4835b1ef88b19848882c591a87d497870b5f2f19
DongNyoung Lee <leedo7182@naver.com>
2026-08-30 20:49:59 +0900

    [level 2] Title: 전화번호 목록, Time: 187.59 ms, Memory: 234 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,12 +2,24 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
-        for(int i=0;i<phone_book.length-1;i++){
-            String pre = phone_book[i];
-            if(phone_book[i+1].startsWith(pre)){
-                return false;
+        
+        
+        //set에 넣기
+        Set<String> set = new HashSet<>();
+        for(String p : phone_book){
+            set.add(p);
+        }
+        
+        for(String p: phone_book){
+            int len = p.length();
+            for(int i=1;i<len;i++){
+                String prefix = p.substring(0,i);
+                //System.out.println(prefix);
+                if(set.contains(prefix)){
+                    return false;
+                }
             }
+            
         }
         return answer;
     }
