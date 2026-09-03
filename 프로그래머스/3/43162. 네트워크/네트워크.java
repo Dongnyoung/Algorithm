60ff03e00138e480dc58e8a8ccfad8bf81582b14
DongNyoung Lee <leedo7182@naver.com>
2026-09-04 01:29:07 +0900

    [level 3] Title: 네트워크, Time: 0.52 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,39 +1,48 @@
+import java.util.*;
 class Solution {
-    public static int[] parent;
+    static List<Integer>[] list;
+    static boolean visited[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        int edges=0;
-        parent = new int[n];
+        list = new ArrayList[n];
+        visited = new boolean[n];
         for(int i=0;i<n;i++){
-            parent[i]=i;
+            list[i]= new ArrayList<>();
         }
         for(int i=0;i<computers.length;i++){
+            //1을 찾아야하는데 [i][i]는 패쓰
             for(int j=0;j<computers[i].length;j++){
-                
-                if(i!=j && computers[i][j]==1){
-                    union(i,j);
-                    
-                    
+                if(i==j){
+                    continue;
+                }
+                if(computers[i][j]==1){
+                    list[i].add(j);
+                    list[j].add(i);
                 }
             }
-            
         }
         for(int i=0;i<n;i++){
-            if(find(i)==i){
+            if(!visited[i]){
+                dfs(i);
                 answer++;
             }
         }
+        
         return answer;
     }
-    public static int find(int x){
-        if(parent[x]==x){
-            return x;
+    
+    //dfs는 즉, 연결된 트리에서 노드의 개수를 리턴하고 전체에서 그 개수를 빼면 남은게 나오고 남은거에 +1하면됨
+    static private int dfs(int computer){
+        int cnt=1;
+        visited[computer] =true;
+        for(int next: list[computer]){
+            if(!visited[next]){
+                
+                int childCnt = dfs(next);
+                cnt+=childCnt;
+            }
+            
         }
-        return parent[x]=find(parent[x]);
-    }
-    public static int union(int x,int y){
-        int root1 = find(x);
-        int root2 = find(y);
-        return parent[root2] = root1;
+        return cnt;
     }
 }
